313860351a98f60e3c014f5b480191c4ecb6dad5
omgawroo98 <omar.ahmed8856@gmail.com>
2025-10-19 21:02:43 +0200

    Add translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   i18n: {
-    locales: ["es", "en", "pt-br"],
+    locales: ["de", "en", "fr", "it"],
     defaultLocale: "en",
+    routing: {
+      // Default = false → URLs in default locale have no /en prefix.
+      // /about, /fr/about, /de/about
+      prefixDefaultLocale: false,
+    }
   },
   site: 'https://omgawroo98.github.io',
   base: '/',
